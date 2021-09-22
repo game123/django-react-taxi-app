49de9f9f8ab89057ba40b2aa7acdaada0ecdfdba
Kevin Chan <kevinykchan@gmail.com>
2021-09-22 12:33:22 -0400

    Updated Media Files settings

--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -101,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
